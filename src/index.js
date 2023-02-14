48d03540e7429161d7f85a8cff0ae54b02fbb7f7
TakenHara <takenhara@gmail.com>
2023-02-14 03:10:01 +0200

    Pony Digital

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import './index.html';
+import './index.scss';
+import 'swiper/scss';
+
+import Swiper, {Navigation, Pagination} from 'swiper';
+
+const swiper = new Swiper(".swiper", {
+	modules: [ Navigation, Pagination ],
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	mousewheel: true,
+	keyboard: true,
+	slidesPerView: 1,
+	spaceBetween: 30,
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
+			spaceBetween: 30
+		},
+		400: {
+			slidesPerView: 2,
+			spaceBetween: 30
+		},
+		640: {
+			slidesPerView: 3,
+			spaceBetween: 30
+		},
+		970: {
+			slidesPerView: 4,
+			spaceBetween: 30
+		}
+	}
+});
