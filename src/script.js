1a7f4ec8c4c5ac0d663999dd5918e160f406a9cd
TakenHara <takenhara@gmail.com>
2023-02-14 03:10:08 +0200

    Pony Digital

---
+++ src/script.js
@@ -0,0 +1,1 @@
+const burger = document.querySelector('.burger');const menu = document.querySelector('.menu');function toggleBurgerMenu() {	burger.classList.toggle('burger_active')	burger.classList.contains('burger_active')			? menu.classList.add('menu_show')			: menu.classList.remove('menu_show')}burger.addEventListener('click', toggleBurgerMenu)
